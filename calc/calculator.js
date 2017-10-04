cfe461705a14e8d3cd2d8fd2a42863a96dcb752c
shane <sdrafahl@iastate.edu>
2017-10-04 12:25:44 -0500

    implemented division

--- calc/calculator.js
+++ calc/calculator.js
@@ -5,26 +5,58 @@ window.onload = function() {
     button.onclick = function() { updateScreen(x) }
   }
   button = document.getElementById("=")
+
   button.onclick = function() {
     textScreen = document.getElementById("input")
     val = compute(textScreen.value)
     textScreen.value = val
   }
+
+  button = document.getElementById("C")
+  button.onclick = function() {
+    textScreen = document.getElementById("input")
+    textScreen.value = ""
+  }
+
+  button = document.getElementById("+")
+  button.onclick = function() {
+    textScreen = document.getElementById("input")
+    textScreen.value = textScreen.value + "+ "
+  }
+
+  button = document.getElementById("-")
+  button.onclick = function() {
+    textScreen = document.getElementById("input")
+    textScreen.value = textScreen.value + "- "
+  }
+
+  button = document.getElementById("*")
+  button.onclick = function() {
+    textScreen = document.getElementById("input")
+    textScreen.value = textScreen.value + "* "
+  }
+
+  button = document.getElementById("/")
+  button.onclick = function() {
+    textScreen = document.getElementById("input")
+    textScreen.value = textScreen.value + "/ "
+  }
+
 }
 
 function updateScreen(string) {
   textScreen = document.getElementById("input")
-  textScreen.value = textScreen.value + string
+  textScreen.value = textScreen.value + string + " "
 }
 
 function compute(infix) {
   post = shuntingYard(infix)
+  console.log(post)
   val = computePostFix(post)
   return val
 }
 
 function shuntingYard(infix) {
-  infix += " "
   output = [] // queue
   operator = [] // stack
 
@@ -32,22 +64,21 @@ function shuntingYard(infix) {
     x = 0
     pre = false
     while(x < infix.length) {
-      if((infix.charAt(x) == " " || infix.charAt(x) == "+" || infix.charAt(x) == "-" || infix.charAt(x) == "*") && character != " " && character != "" ) {
+      if((infix.charAt(x) == " " || infix.charAt(x) == "+" || infix.charAt(x) == "-" || infix.charAt(x) == "*" || infix.charAt(x) == "/") && character != " " && character != "" ) {
         output.push(character)
         if(pre) {
           output.push(operator.pop())
         }
         character = ""
       } else {
-        if(infix.charAt(x) == "+" || infix.charAt(x) == "-" || infix.charAt(x) == "*") {
-          if(infix.charAt(x) == "*") {
+        if(infix.charAt(x) == "+" || infix.charAt(x) == "-" || infix.charAt(x) == "*" || infix.charAt(x) == "/") {
+          if(infix.charAt(x) == "*" || infix.charAt(x) == "/") {
             pre = true
           }
           operator.push(infix.charAt(x))
-          console.log(operator)
         }
       }
-      if(infix.charAt(x) != " " && infix.charAt(x) != "+" && infix.charAt(x) != "-" && infix.charAt(x) != "*") {
+      if(infix.charAt(x) != " " && infix.charAt(x) != "+" && infix.charAt(x) != "-" && infix.charAt(x) != "*" && infix.charAt(x) != "/") {
         character += infix.charAt(x)
       }
       x++
@@ -65,11 +96,11 @@ function computePostFix(postFix) {
   length = postFix.length
   for(x = 0; x < length ; x++) {
     charactor = postFix[x]
-    if(charactor == "+" || charactor == "*" || charactor == "-") {
+    if(charactor == "+" || charactor == "*" || charactor == "-" || charactor == "/") {
       val1 = stack.pop()
       val2 = stack.pop()
-      val1 = parseInt(val1)
-      val2 = parseInt(val2)
+      val1 = val1 * 1.0
+      val2 = val2 * 1.0
       if(charactor == "+") {
         stack.push(val1 + val2)
       }
@@ -78,6 +109,9 @@ function computePostFix(postFix) {
       }
       if(charactor == "*") {
         stack.push(val1 * val2)
+      }
+      if(charactor == "/") {
+        stack.push(val2 / val1)
       }
     } else {
       stack.push(charactor)
